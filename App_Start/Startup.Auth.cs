af0253fac724e33c20ea1fcd92f2d6d59daf920e
Neutrón 117 <guerrerooctavio80@gmail.com>
2024-10-25 21:19:43 -0500

    Agregar archivos de proyecto.

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using Redes_Sociales_4B.Models;
+
+namespace Redes_Sociales_4B
+{
+    public partial class Startup
+    {
+       
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+
+
+
+
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                     
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });     
+            
+
+
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+
+           
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+
+
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+          
+
+            app.UseFacebookAuthentication
+             (
+
+            appId: "1077662459945197",
+            appSecret: "f280ab82fe03eee84f8888112f62f956"
+            
+              );
+
+        }
+    }
+}
